f6d10cb030c5372a4868abcc344d09f2cad8a819
Hasban-Fardani <hasbanfardani77@gmail.com>
2023-12-05 22:07:13 +0700

    finishing

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
     extend: {
       boxShadow: {
         '3xl': '0px 0px 50px -5px rgba(0,0,0,0.4)',
+      },
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif']
       }
     },
   },
